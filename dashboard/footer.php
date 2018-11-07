ae86d47e0d0ea6d90c829b0688c885139c19d431
Meta Wahyudi <metawahyudi8@gmail.com>
2018-11-07 16:18:31 +0700

    perubahan baris ke 3 comel

--- dashboard/footer.php
+++ dashboard/footer.php
@@ -1,5 +1,5 @@
 <footer>
 			<div class="container-fluid">
-				<p class="copyright">&copy; 2018 Popeye Team | Sehatin</p>
+				<p class="copyright">&copy; 2018 Popeye Team | Sehatin Comel</p>
 			</div>
 </footer>
