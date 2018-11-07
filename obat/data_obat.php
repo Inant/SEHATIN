2e19ea7fb926ac5d9f28eab1952ae8772925d8e5
Inant Kharisma <inant.kharisma@gmail.com>
2018-11-07 14:55:01 +0700

    data obat

--- obat/data_obat.php
+++ obat/data_obat.php
@@ -97,6 +97,7 @@ if (empty($_SESSION['username']) && empty($_SESSION['level'])) {
 																		</td>
 																	</tr>";
 																	$no++;
+																	
 													}
 												 ?>
 											</tbody>
