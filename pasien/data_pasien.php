e3517432f5c1811617bc6db2cceaa895fa341375
Meta Wahyudi <metawahyudi8@gmail.com>
2018-11-08 08:01:13 +0700

    Perubahan 12

--- pasien/data_pasien.php
+++ pasien/data_pasien.php
@@ -9,7 +9,7 @@ if (empthy($_SESSION['username'])&& empty($_SESSION['level'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Data Pasien | Sehatin</title>
+	<title>Data Pasien | Sehatin Meta</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, uer-scalable=0">
