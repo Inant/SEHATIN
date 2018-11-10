7ef4c9c3616ac0bc11712aa75a34c409e0a0bf3a
Elsa <elsaramadania@gmail.com>
2018-11-10 10:06:47 +0700

    pasien mahasiswa

--- pasien/pasien_karyawan.php
+++ pasien/pasien_karyawan.php
@@ -9,7 +9,7 @@ if (empty($_SESSION['username']) && empty($_SESSION['level'])) {
 <!doctype html>
 <html lang="en">
 <head>
-	<title>Pasien Umum | Sehatin</title>
+	<title>Pasien Karyawan | Sehatin</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
